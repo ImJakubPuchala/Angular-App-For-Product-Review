d99906cdc4b2bf4810f15002f2818150b3447e96
Jakub Puchala <86433812+ImJakubPuchala@users.noreply.github.com>
2024-02-14 20:42:55 +0100

    handleError function

--- src/app/Service/product.service.ts
+++ src/app/Service/product.service.ts
@@ -1,24 +1,42 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
+import { Observable, throwError } from 'rxjs';
+import { catchError } from 'rxjs/operators';
 import { ProductInformation } from '../Model/ProductInformation';
 
 @Injectable({
   providedIn: 'root',
 })
 export class ProductService {
-  private apiUrl = "https://localhost:7204";
+  private apiUrl = 'https://localhost:7204';
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient) {}
 
   searchProducts(name: string): Observable<any[]> {
-    return this.http.get<ProductInformation[]>(`${this.apiUrl}/Review/SearchProductByName?name=${name}`);
+    return this.http
+      .get<ProductInformation[]>(
+        `${this.apiUrl}/Review/SearchProductByName?name=${name}`
+      )
+      .pipe(catchError(this.handleError));
   }
 
   addReview(productId: number, rating: number): Observable<any> {
-    return this.http.post(`${this.apiUrl}/Review/AddReview`, {
-      ProductId: productId,
-      Rating: rating,
-    });
+    return this.http
+      .post(`${this.apiUrl}/Review/AddReview`, {
+        ProductId: productId,
+        Rating: rating,
+      })
+      .pipe(catchError(this.handleError));
+  }
+
+  private handleError(error: HttpErrorResponse) {
+    let errorMessage = 'Unknown error!';
+    if (error.error instanceof ErrorEvent) {
+      errorMessage = `Error: ${error.error.message}`;
+    } else {
+      errorMessage = `Error Code: ${error.status}\nMessage: ${error.message}`;
+    }
+    console.error(errorMessage);
+    return throwError(errorMessage);
   }
 }
